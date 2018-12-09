ecea1412528624bbf7df6dcb31e3978c9c3d2237
hank <henrik@hankolsen.com>
2018-12-09 23:07:02 +0100

    Fix broken refactoring

--- 2018/09/marble.js
+++ 2018/09/marble.js
@@ -1,45 +1,38 @@
-let score;
-let current = { value: 0 };
-current.prev = current;
-current.next = current;
-
-/* eslint-disable no-param-reassign */
-/* eslint-disable-next-line object-curly-newline */
-const putMarble = ({ player, currentPoint }) => {
-  let target;
-
-  if (currentPoint % 23) {
-    target = current.next;
-    const toAdd = { value: currentPoint, prev: target, next: target.next };
-    target.next.prev = toAdd;
-    target.next = toAdd;
-    current = toAdd;
-  } else {
-    target = current;
-    for (let i = 0; i < 7; i += 1) {
-      target = target.prev;
-    }
-    const points = currentPoint + target.value;
-    score[player] += points;
-    target.next.prev = target.prev;
-    target.prev.next = target.next;
-    current = target.next;
-  }
-
-  return { current, score };
-};
-/* eslint-enable no-param-reassign */
-
 const getHighScore = (players, lastPoint) => {
   let currentPoint = 0;
-  let player = -1;
-  score = Array(players).fill(0);
 
+  const score = Array(players).fill(0);
+  let current = { value: 0 };
+  current.prev = current;
+  current.next = current;
+
+  const putMarble = (player) => {
+    let target;
+
+    if (currentPoint % 23) {
+      target = current.next;
+      const toAdd = { value: currentPoint, prev: target, next: target.next };
+      target.next.prev = toAdd;
+      target.next = toAdd;
+      current = toAdd;
+    } else {
+      target = current;
+      for (let i = 0; i < 7; i += 1) {
+        target = target.prev;
+      }
+      const points = currentPoint + target.value;
+      score[player] += points;
+      target.next.prev = target.prev;
+      target.prev.next = target.next;
+      current = target.next;
+    }
+  };
+
+  let player = -1;
   while (currentPoint < lastPoint) {
     currentPoint += 1;
     player = (player + 1) % players;
-    /* eslint-disable-next-line object-curly-newline */
-    putMarble({ player, currentPoint });
+    putMarble(player);
   }
 
   return Math.max.apply(null, score);
