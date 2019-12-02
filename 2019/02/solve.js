deec48e16f080c8c641b9125bd110f210e28c423
hank <henrik@hankolsen.com>
2019-12-02 22:28:29 +0100

    Refactoring and renaming things

--- 2019/02/solve.js
+++ 2019/02/solve.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env/node
 const { getRow } = require('../../utils');
 
-const add = (arr, currentIndex) => {
-  arr[arr[currentIndex + 3]] = arr[arr[currentIndex + 1]] + arr[arr[currentIndex + 2]];
-  return arr;
+const getParams = (program, position) => program.slice(position, position + 4);
+
+const add = (program, position) => {
+  const [, param1, param2, output] = getParams(program, position);
+  program[output] = program[param1] + program[param2];
+  return program;
 };
 
-const multiply = (arr, currentIndex) => {
-  arr[arr[currentIndex + 3]] = arr[arr[currentIndex + 1]] * arr[arr[currentIndex + 2]];
-  return arr;
+const multiply = (program, position) => {
+  const [, param1, param2, output] = getParams(program, position);
+  program[output] = program[param1] * program[param2];
+  return program;
 };
 
 const operations = {
@@ -38,17 +42,14 @@ const solve1 = () => {
 const solve2 = () => {
   getRow()
     .then((row) => {
+      const input = row.split(',').map(i => Number(i));
       const goal = 19690720;
 
       const nouns = Array(100)
         .fill(1)
         .map((_, i) => i);
 
-      const verbs = Array(100)
-        .fill(1)
-        .map((_, i) => i);
-
-      const input = row.split(',').map(i => Number(i));
+      const verbs = [...nouns];
 
       nouns.some((noun) => verbs
         .some((verb) => {
