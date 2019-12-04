88d956d13ad889f8969ad035b32da0a7b3ff7645
hank <henrik@hankolsen.com>
2019-12-04 19:19:22 +0100

    Add Part 2 of Day 4

--- 2019/04/solve.js
+++ 2019/04/solve.js
@@ -31,7 +31,24 @@ const isAdjacent = (i) => {
   });
 };
 
+const isAdjacentPart2 = (i) => {
+  const numbers = getNumbers(i);
+  // eslint-disable-next-line prefer-const
+  let [current, ...rest] = numbers;
+  const doubles = {};
+
+  rest.forEach((number) => {
+    if (current === number) {
+      doubles[current] = doubles[current] ? doubles[current] += 1 : 1;
+    }
+    current = number;
+  });
+
+  return Object.values(doubles).some((double) => double === 1);
+};
+
 const isValid = (i) => isIncreasing(i) && isAdjacent(i);
+const isValidPart2 = (i) => isIncreasing(i) && isAdjacentPart2(i);
 
 const solve1 = () => {
   const start = 359282;
@@ -48,10 +65,19 @@ const solve1 = () => {
 };
 
 const solve2 = () => {
+  const start = 359282;
+  const end = 820401;
+  let matches = 0;
 
+  for (let i = start; i <= end; i += 1) {
+    if (isValidPart2(i)) {
+      matches += 1;
+    }
+  }
+  console.log(`Part 2: ${matches}`);
 };
 
 solve1();
 solve2();
 
-module.exports = { isValid };
+module.exports = { isValid, isValidPart2 };
--- 2019/04/test.js
+++ 2019/04/test.js
@@ -1,9 +1,15 @@
-const { isValid } = require('./solve');
+const { isValid, isValidPart2 } = require('./solve');
 
 describe('Day 4', () => {
   it('should solve part 1 examples', () => {
     expect(isValid('111111')).toBe(true);
     expect(isValid('223450')).toBe(false);
     expect(isValid('123789')).toBe(false);
+  });
+
+  it('should solve part 2 examples', () => {
+    expect(isValidPart2('112233')).toBe(true);
+    expect(isValidPart2('123444')).toBe(false);
+    expect(isValidPart2('111122')).toBe(true);
   });
 });
