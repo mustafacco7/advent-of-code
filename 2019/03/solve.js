19ec1eac9d350ef7bd4e0c8052f6df83d9293b1f
hank <henrik@hankolsen.com>
2019-12-08 22:21:36 +0100

    Refactor Day 3

--- 2019/03/solve.js
+++ 2019/03/solve.js
@@ -2,83 +2,71 @@
 
 const { getRows } = require('../../utils');
 
-const getInstructions = (rows) => rows.map((row) => row.split(','));
+const getMoves = (wires) => wires
+  .map((wire) => wire.split(',')
+    .map(move => move.match(/([RLDU])(\d+)/u))
+    .map(([, direction, distance]) => ({ direction, distance: Number(distance) })));
 
-const getLines = (instructions) => {
-  let horizontalLines = [];
-  let verticalLines = [];
-  const coordinate = [0, 0];
-  instructions.forEach((instruction) => {
-    const [, direction, length] = instruction.match(/([RLDU])(\d+)/);
-    if (direction === 'L' || direction === 'R') {
-      const distance = direction === 'R' ? Number(length) : -Number(length);
-      const x1 = Math.min(coordinate[0], coordinate[0] + distance);
-      const x2 = Math.max(coordinate[0], coordinate[0] + distance);
-      horizontalLines = [...horizontalLines, { y: coordinate[1], x1, x2 }];
-      coordinate[0] += distance;
-    }
-    if (direction === 'U' || direction === 'D') {
-      const distance = direction === 'D' ? Number(length) : -Number(length);
-      const y1 = Math.min(coordinate[1], coordinate[1] + distance);
-      const y2 = Math.max(coordinate[1], coordinate[1] + distance);
-      verticalLines = [...verticalLines, { x: coordinate[0], y1, y2 }];
-      coordinate[1] += distance;
-    }
-  });
-
-  return { horizontalLines, verticalLines };
+const directions = {
+  U: { x: 0, y: -1 },
+  D: { x: 0, y: 1 },
+  R: { x: 1, y: 0 },
+  L: { x: -1, y: 0 },
 };
 
-const getMinDistance = (instructions) => {
-  const { horizontalLines, verticalLines } = getLines(instructions[0]);
-  const coordinate = [0, 0];
-  let matches = [];
-  instructions[1].forEach((instruction) => {
-    const [, direction, length] = instruction.match(/([RLDU])(\d+)/);
-    if (direction === 'L' || direction === 'R') {
-      const distance = direction === 'R' ? Number(length) : -Number(length);
-      const x1 = Math.min(coordinate[0], coordinate[0] + distance);
-      const x2 = Math.max(coordinate[0], coordinate[0] + distance);
-      const matchingLines = verticalLines.filter((line) => line.x > x1 && line.x < x2
-        && line.y1 < coordinate[1] && line.y2 > coordinate[1]);
-      if (matchingLines.length) {
-        matches = [...matches, [matchingLines[0].x, coordinate[1]]];
-      }
-      coordinate[0] += distance;
-    }
-    if (direction === 'U' || direction === 'D') {
-      const distance = direction === 'D' ? Number(length) : -Number(length);
-      const y1 = Math.min(coordinate[1], coordinate[1] + distance);
-      const y2 = Math.max(coordinate[1], coordinate[1] + distance);
-      const matchingLines = horizontalLines.filter((line) => line.y > y1 && line.y < y2
-        && line.x1 < coordinate[0] && line.x2 > coordinate[0]);
-      if (matchingLines.length) {
-        matches = [...matches, [coordinate[0], matchingLines[0].y]];
-      }
-      coordinate[1] += distance;
-    }
+const key = ({ x, y }) => `${x},${y}`;
+
+const plotWires = (moves) => {
+  const wires = {};
+  moves.forEach((instructions, wire) => {
+    const position = { x: 0, y: 0 };
+    instructions.forEach(({ direction, distance }) => {
+      Array(distance).fill(1).forEach(() => {
+        const { x, y } = directions[direction];
+        position.x += x;
+        position.y += y;
+        // We don't count intersections between the same wire
+        if (wires[key(position)] && wires[key(position)].includes(wire)) {
+          return;
+        }
+        wires[key(position)] = wires[key(position)] ? [...wires[key(position)], wire] : [wire];
+      });
+    });
   });
-  const distances = matches.map(([x, y]) => Math.abs(x) + Math.abs(y));
+
+  return wires;
+};
+
+const findIntersections = (wires) => Object
+  .entries(wires)
+  .filter(([, hits]) => hits.length > 1)
+  .map(([coordinate]) => coordinate
+    .split(',')
+    .map(Number));
+
+const getMinDistance = (instructions) => {
+  const moves = getMoves(instructions);
+  const wires = plotWires(moves);
+  const intersections = findIntersections(wires);
+  const distances = intersections.map(([x, y]) => Math.abs(x) + Math.abs(y));
   return Math.min(...distances);
 };
 
 const solve1 = () => {
   getRows()
     .then((rows) => {
-      const instructions = getInstructions(rows);
-      const minDistance = getMinDistance(instructions);
-      console.log(minDistance);
+      console.log(`Part 1: ${getMinDistance(rows)}`);
     });
 };
 
 const solve2 = () => {
   getRows()
     .then((rows) => {
-      const instructions = getInstructions(rows);
+      const instructions = getMoves(rows);
     });
 };
 
 solve1();
 solve2();
 
-module.exports = { getInstructions, getMinDistance };
+module.exports = { getMinDistance };
--- 2019/03/test.js
+++ 2019/03/test.js
@@ -1,4 +1,4 @@
-const { getInstructions, getMinDistance } = require('./solve');
+const { getMinDistance } = require('./solve');
 
 describe('Day 3', () => {
   test('it should solve part 1 test cases', () => {
@@ -7,19 +7,41 @@ describe('Day 3', () => {
       'R8,U5,L5,D3',
       'U7,R6,D4,L4',
     ];
-    expect(getMinDistance(getInstructions(input))).toEqual(6);
+    expect(getMinDistance(input)).toEqual(6);
 
     const input1 = [
       'R75,D30,R83,U83,L12,D49,R71,U7,L72',
       'U62,R66,U55,R34,D71,R55,D58,R83',
     ];
-    expect(getMinDistance(getInstructions(input1))).toEqual(159);
+    expect(getMinDistance(input1)).toEqual(159);
 
     const input2 = [
       'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
       'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
     ];
-    expect(getMinDistance(getInstructions(input2))).toEqual(135);
+    expect(getMinDistance(input2)).toEqual(135);
 
+  });
+
+  test('it should solve part 2 test cases', () => {
+
+    /*const input = [
+      'R8,U5,L5,D3',
+      'U7,R6,D4,L4',
+    ];
+    expect(getMinSteps(getInstructions(input))).toEqual(30);
+
+    const input1 = [
+      'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+      'U62,R66,U55,R34,D71,R55,D58,R83',
+    ];
+    expect(getMinSteps(getInstructions(input1))).toEqual(610);
+
+    const input2 = [
+      'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+      'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
+    ];
+    expect(getMinSteps(getInstructions(input2))).toEqual(410);
+*/
   });
 });
