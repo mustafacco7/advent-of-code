673f7c93e82b72298a1c6369fb16602a4ceefd92
hank <henrik@hankolsen.com>
2019-12-12 17:43:31 +0100

    Add Day 12 step 1

---
+++ 2019/12/solve.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env/node
+
+const { getRows } = require('../../utils');
+
+const calculateMoonPotentialEnergy = (moon) => {
+  const { x, y, z } = moon.position;
+  return Math.abs(x) + Math.abs(y) + Math.abs(z);
+};
+
+const calculateMoonKineticEnergy = (moon) => {
+  const { x, y, z } = moon.velocity;
+  return Math.abs(x) + Math.abs(y) + Math.abs(z);
+};
+
+const calculateMoonEnergy = (moon) => calculateMoonKineticEnergy(moon) * calculateMoonPotentialEnergy(moon);
+
+const calculateTotalMoonEnergy = (moons) => moons
+  .reduce((sum, moon) => {
+    sum += calculateMoonEnergy(moon);
+    return sum;
+  }, 0);
+
+const calculatePositions = (moons, velocities) => moons
+  .map((moon, index) => ['x', 'y', 'z']
+    .reduce((position, axis) => {
+      position[axis] += velocities[index][axis];
+      return position;
+    }, moon.position));
+
+const getDelta = (a, b) => {
+  if (a === b) {
+    return 0;
+  }
+  if (a > b) {
+    return 1;
+  }
+  return -1;
+};
+
+const calculateVelocities = (moons) => moons
+  .map((moon, index) => {
+    const otherMoons = [...moons];
+    otherMoons.splice(index, 1);
+    return otherMoons.reduce(({ x, y, z }, otherMoon) => {
+      x += getDelta(otherMoon.position.x, moon.position.x);
+      y += getDelta(otherMoon.position.y, moon.position.y);
+      z += getDelta(otherMoon.position.z, moon.position.z);
+      return { x, y, z };
+    }, { x: moon.velocity.x, y: moon.velocity.y, z: moon.velocity.z });
+  });
+
+const calculateNextStep = (moons) => {
+  const velocities = calculateVelocities(moons);
+  const positions = calculatePositions(moons, velocities);
+  moons.map((moon, index) => {
+    moon.position = positions[index];
+    moon.velocity = velocities[index];
+    return moon;
+  });
+
+  return moons;
+};
+
+const solve1 = () => {
+  getRows()
+    .then((rows) => {
+      let moons = rows.map((row) => {
+        const [, x, y, z] = row.match(/x=(-?\d+), y=(-?\d+), z=(-?\d+)/);
+        return {
+          position: {
+            x: Number(x),
+            y: Number(y),
+            z: Number(z),
+          },
+          velocity: {
+            x: 0,
+            y: 0,
+            z: 0,
+          },
+        };
+      });
+      const steps = 1000;
+
+      Array(steps).fill(1).forEach(() => {
+        moons = calculateNextStep(moons);
+      });
+
+      console.log(`Part 1: ${calculateTotalMoonEnergy(moons)}`);
+    });
+};
+
+const solve2 = () => {
+  getRows()
+    .then((rows) => {
+      // console.log(rows);
+    });
+};
+
+solve1();
+solve2();
+
+module.exports = { calculateMoonEnergy, calculateNextStep, calculatePositions, calculateVelocities };
---
+++ 2019/12/test.js
@@ -0,0 +1,89 @@
+const { calculateMoonEnergy, calculateNextStep, calculatePositions, calculateVelocities } = require('./solve');
+
+describe('Day 2', () => {
+
+  let moons = [];
+  let velocities = [];
+
+  beforeEach(() => {
+    moons = [
+      { position: { x: -1, y: 0, z: 2 }, velocity: { x: 0, y: 0, z: 0 } },
+      { position: { x: 2, y: -10, z: -7 }, velocity: { x: 0, y: 0, z: 0 } },
+      { position: { x: 4, y: -8, z: 8 }, velocity: { x: 0, y: 0, z: 0 } },
+      { position: { x: 3, y: 5, z: -1 }, velocity: { x: 0, y: 0, z: 0 } },
+    ];
+  });
+
+
+  test('it should calculate correct velocities after step 1', () => {
+    velocities = calculateVelocities(moons);
+    expect(velocities).toEqual([
+      { x: 3, y: -1, z: -1 },
+      { x: 1, y: 3, z: 3 },
+      { x: -3, y: 1, z: -3 },
+      { x: -1, y: -3, z: 1 },
+    ]);
+  });
+
+  test('it should calculate correct positions after step 1', () => {
+    const positionsStep1 = calculatePositions([...moons], velocities);
+    expect(positionsStep1).toEqual([
+      { x: 2, y: -1, z: 1 },
+      { x: 3, y: -7, z: -4 },
+      { x: 1, y: -7, z: 5 },
+      { x: 2, y: 2, z: 0 },
+    ]);
+  });
+
+  test('it should calculate correct state after step 1', () => {
+    expect(calculateNextStep(moons)).toEqual([
+      { position: { x: 2, y: -1, z: 1 }, velocity: { x: 3, y: -1, z: -1 } },
+      { position: { x: 3, y: -7, z: -4 }, velocity: { x: 1, y: 3, z: 3 } },
+      { position: { x: 1, y: -7, z: 5 }, velocity: { x: -3, y: 1, z: -3 } },
+      { position: { x: 2, y: 2, z: 0 }, velocity: { x: -1, y: -3, z: 1 } },
+    ]);
+  });
+
+  test('it should calculate correct velocities after step 2', () => {
+    const moonsAfterStep1 = [
+      { position: { x: 2, y: -1, z: 1 }, velocity: { x: 3, y: -1, z: -1 } },
+      { position: { x: 3, y: -7, z: -4 }, velocity: { x: 1, y: 3, z: 3 } },
+      { position: { x: 1, y: -7, z: 5 }, velocity: { x: -3, y: 1, z: -3 } },
+      { position: { x: 2, y: 2, z: 0 }, velocity: { x: -1, y: -3, z: 1 } },
+    ];
+
+    const velocitiesAfterStep2 = calculateVelocities(moonsAfterStep1);
+
+    expect(velocitiesAfterStep2).toEqual([
+      { x: 3, y: -2, z: -2 },
+      { x: -2, y: 5, z: 6 },
+      { x: 0, y: 3, z: -6 },
+      { x: -1, y: -6, z: 2 },
+    ]);
+  });
+
+  let moonsAfterStep10;
+  test('it should calculate correct state after step 10', () => {
+    Array(10).fill(1).forEach(() => {
+      moons = calculateNextStep(moons);
+    });
+
+    moonsAfterStep10 = moons;
+
+    expect(moons).toEqual([
+      { position: { x: 2, y: 1, z: -3 }, velocity: { x: -3, y: -2, z: 1 } },
+      { position: { x: 1, y: -8, z: 0 }, velocity: { x: -1, y: 1, z: 3 } },
+      { position: { x: 3, y: -6, z: 1 }, velocity: { x: 3, y: 2, z: -3 } },
+      { position: { x: 2, y: 0, z: 4 }, velocity: { x: 1, y: -1, z: -1 } },
+    ]);
+  });
+
+  test('it should calculate correct energy after step 10', () => {
+    const totalMoonEnergy = moonsAfterStep10.reduce((sum, moon) => {
+      sum += calculateMoonEnergy(moon);
+      return sum;
+    }, 0);
+
+    expect(totalMoonEnergy).toEqual(179);
+  });
+});
