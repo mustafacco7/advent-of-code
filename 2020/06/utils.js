4ee6c85f2120939b0300c8673570b779589dc650
hank <henrik@hankolsen.com>
2020-12-06 13:26:30 +0100

    Add Day 6 part 2

--- 2020/06/utils.js
+++ 2020/06/utils.js
@@ -1,4 +1,4 @@
-const getUniqueAnswers = (input) =>
+const getGroupedAnswers = (input) =>
   input.reduce(
     (acc, row) => {
       if (row) {
@@ -9,7 +9,10 @@ const getUniqueAnswers = (input) =>
       return acc;
     },
     [[]],
-  ).map((row) => [...new Set(row.join(''))]);
+  );
+
+const getUniqueAnswers = (input) =>
+  getGroupedAnswers(input).map((row) => [...new Set(row.join(''))]);
 
 const util1 = (input) => {
   const totalAnswers = getUniqueAnswers(input).reduce((sum, groupAnswers) => {
@@ -20,8 +23,31 @@ const util1 = (input) => {
 };
 
 const util2 = (input) => {
-  console.log(input);
-  return input;
+  const groups = getGroupedAnswers(input);
+  const groupMembers = groups.map((group) => group.length);
+  const sum = groups.reduce((validAnswersCount, group, i) => {
+    if (groupMembers[i] === 1) {
+      validAnswersCount += group[0].length;
+      return validAnswersCount;
+    }
+
+    const groupAnswersCount = group.reduce((answersCount, member) => {
+      member.split('').forEach((answer) => {
+        answersCount[answer] = answersCount[answer] ? (answersCount[answer] += 1) : 1;
+      });
+      return answersCount;
+    }, {});
+
+    validAnswersCount += Object.values(groupAnswersCount).reduce((total, answers) => {
+      if (answers >= groupMembers[i]) {
+        total += 1;
+      }
+      return total;
+    }, 0);
+
+    return validAnswersCount;
+  }, 0);
+  return sum;
 };
 
 module.exports = { util1, util2 };
