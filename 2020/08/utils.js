f97885ce0a2932032884e71a2fc0b81360c2955c
Hank <hank@hank.se>
2020-12-08 13:20:00 +0100

    Add day 8. part 2

--- 2020/08/utils.js
+++ 2020/08/utils.js
@@ -37,9 +37,49 @@ const util1 = (input) => {
   return acc;
 };
 
+const changed = {};
+
+const getOp = ({ instructions, pointer, changeDone }) => {
+  const { op, value } = instructions[pointer];
+  let realOp = op;
+  if (!changeDone && !changed[pointer]) {
+    if (op === 'jmp') {
+      realOp = 'nop';
+      changeDone = true;
+    } else if (op === 'nop' && value !== 0) {
+      realOp = 'jmp';
+      changeDone = true;
+    }
+    changed[pointer] = true;
+  }
+
+  return { op: realOp, value, changeDone };
+};
+
 const util2 = (input) => {
-  console.log(input);
-  return input;
+  let pointer = 0;
+  let acc;
+  const instructions = parseInstructions(input);
+  while (pointer < instructions.length) {
+    pointer = 0;
+    acc = 0;
+    const visited = {};
+    let changeDone = false;
+    while (!visited[pointer] && pointer < instructions.length) {
+      const { op, value, changeDone: done } = getOp({
+        instructions,
+        pointer,
+        changeDone,
+      });
+      changeDone = done;
+      visited[pointer] = true;
+      const result = computer[op]({ pointer, value, acc });
+      acc = result.acc;
+      pointer = result.pointer;
+    }
+  }
+
+  return acc;
 };
 
 module.exports = { util1, util2 };
